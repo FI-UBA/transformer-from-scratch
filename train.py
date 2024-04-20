049772f6b4e54a042b04227daf5068b9ee6670f4
Francisco GENTILE <ingentile@gmail.com>
2024-04-20 15:12:12 -0300

    - Bug Fixed: Changed save_every for save_interval

--- train.py
+++ train.py
@@ -311,7 +311,7 @@ def train_model(config):
         run_validation(model, val_dataloader, tokenizer_src, tokenizer_tgt, config['model']['seq_len'], device,
                     lambda msg: batch_iterator.write(msg), global_step, writer, num_examples=config['training']['num_val_samples'])
 
-        if epoch % config['training']['save_every'] == 0:
+        if epoch % config['training']['save_interval'] == 0:
             # Save the model
             model_filename = get_weights_file_path(config, f'{epoch:04d}')
             torch.save({
